cf9ce4ffb38d5a54580a969ee3af159e02b8ce03
Salih1-55 <buenosaires.sy19@gmail.com>
2020-08-16 00:59:47 -0300

    ikinci projemin ilk commit'i

---
+++ src/day01/Deneme.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class Deneme {
+
+	public static void main(String[] args) {
+		System.out.println("Ikinci deneme projem");
+		System.out.println("Ikinci satir");
+
+	}
+
+}
