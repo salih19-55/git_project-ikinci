e73e2c09a416f3982d67c839fb7c980696d89b46
salih19-55 <69353852+salih19-55@users.noreply.github.com>
2020-08-16 01:10:13 -0300

    Update Deneme.java
    git hup'ta degisiklik yaptim

--- src/day01/Deneme.java
+++ src/day01/Deneme.java
@@ -5,7 +5,7 @@ public class Deneme {
 	public static void main(String[] args) {
 		System.out.println("Ikinci deneme projem");
 		System.out.println("Ikinci satir");
-
+                System.out.println("remote degisiklik");
 	}
 
 }
